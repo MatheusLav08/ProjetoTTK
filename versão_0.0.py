b7f88545728efb19555294a211b2499af81823ed
MatheusLav08 <lav.m36@gmail.com>
2026-07-10 20:18:57 -0300

    versão 0.1
    aumentar tamanho da tela

--- versão_0.0.py
+++ versão_0.0.py
@@ -71,8 +71,8 @@ option_menu = ttk.OptionMenu(frame, tipo_figura_var,
 option_menu.grid(column=1, row=0, sticky=W, **paddings)
 
 # Área de desenho
-canvas = Canvas(frame, bg='white', width=600, height=600)
-canvas.grid(column=0, row=1, columnspan=2, sticky=W, **paddings)
+canvas = Canvas(frame, bg='white', width=1280, height=720)
+canvas.grid(column=0, row=1, columnspan=3, sticky=W, **paddings)
 
 frame.pack()
 
